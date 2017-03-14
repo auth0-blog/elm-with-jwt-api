9359f1d43958e5890baac476e48832a76f7b8a8a
Kim Maida <kim.maida@auth0.com>
2017-03-14 19:10:41 -0400

    Initial commit.

---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+var gulp = require('gulp');
+var elm = require('gulp-elm');
+var gutil = require('gulp-util');
+var plumber = require('gulp-plumber');
+var connect = require('gulp-connect');
+
+// File paths
+var paths = {
+  dest: 'dist',
+  elm: 'src/*.elm',
+  static: 'src/*.{html,css}'
+};
+
+// Init Elm
+gulp.task('elm-init', elm.init);
+ 
+// Compile Elm
+gulp.task('elm', ['elm-init'], function(){
+    return gulp.src(paths.elm)
+        .pipe(plumber())
+        .pipe(elm())
+        .pipe(gulp.dest(paths.dest));
+});
+
+// Move static assets to dist
+gulp.task('static', function() {
+    return gulp.src(paths.static)
+        .pipe(plumber())
+        .pipe(gulp.dest(paths.dest));
+});
+
+// Watch for changes and compile
+gulp.task('watch', function() {
+    gulp.watch(paths.elm, ['elm']);
+    gulp.watch(paths.static, ['static']);
+});
+
+// Local server
+gulp.task('connect', function() {
+    connect.server({
+        root: 'dist',
+        port: 3000
+    });
+});
+
+// Main gulp tasks
+gulp.task('build', ['elm', 'static']);
+gulp.task('default', ['connect', 'build', 'watch']);
