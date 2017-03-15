07eedc8befc26bbe08558214ac630af35549171c
Kim Maida <kim.maida@auth0.com>
2017-03-15 12:17:30 -0400

    Step 2 - Fetch a random Chuck Norris quote from the API and display it.

--- src/Main.elm
+++ src/Main.elm
@@ -3,6 +3,7 @@ port module Main exposing (..)
 import Html exposing (..)
 import Html.Events exposing (..)
 import Html.Attributes exposing (..)
+import Http
 
 
 main : Program Never Model Msg
@@ -29,33 +30,84 @@ type alias Model =
     
 init : (Model, Cmd Msg)
 init =
-    ( Model "", Cmd.none )
+    ( Model "", fetchRandomQuoteCmd )
 
 
 
 {-
    UPDATE
+   * API routes
+   * GET
    * Messages
    * Update case
 -}
+-- API request URLs
 
 
-type Msg = GetQuote
+api : String
+api =
+    "http://localhost:3001/"
+
+
+randomQuoteUrl : String
+randomQuoteUrl =
+    api ++ "api/random-quote"
+
+
+
+-- GET a random quote (unauthenticated)
+
+
+fetchRandomQuote : Http.Request String
+fetchRandomQuote =
+    Http.getString randomQuoteUrl
+
+
+fetchRandomQuoteCmd : Cmd Msg
+fetchRandomQuoteCmd =
+    Http.send FetchRandomQuoteCompleted fetchRandomQuote
+
+
+fetchRandomQuoteCompleted : Model -> Result Http.Error String -> ( Model, Cmd Msg )
+fetchRandomQuoteCompleted model result =
+    case result of
+        Ok newQuote ->
+            ( { model | quote = newQuote }, Cmd.none )
+
+        Err _ ->
+            ( model, Cmd.none )    
+
+
+
+-- Messages	
+
+
+type Msg
+    = GetQuote
+    | FetchRandomQuoteSuccess String
+    | FetchRandomQuoteCompleted (Result Http.Error String)
+
+
+-- Update
+
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         GetQuote ->
-            ( { model | quote = model.quote ++ "A quote! " }, Cmd.none )
+            ( model, fetchRandomQuoteCmd )
+
+        FetchRandomQuoteSuccess newQuote ->
+            ( { model | quote = newQuote }, Cmd.none )
+
+        FetchRandomQuoteCompleted result ->
+            fetchRandomQuoteCompleted model result
 
 
 
 {-
    VIEW
-   * Hide sections of view depending on authenticaton state of model
    * Get a quote
-   * Log In or Register
-   * Get a protected quote
 -}
 
 
