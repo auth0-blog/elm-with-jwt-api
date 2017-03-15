dd70715bdd50c7e2af75ba0f79491156a2c5073e
Kim Maida <kim.maida@auth0.com>
2017-03-15 13:57:06 -0400

    Step 3 - Register a User

--- src/Main.elm
+++ src/Main.elm
@@ -4,6 +4,8 @@ import Html exposing (..)
 import Html.Events exposing (..)
 import Html.Attributes exposing (..)
 import Http
+import Json.Decode as Decode exposing (..)
+import Json.Encode as Encode exposing (..)
 
 
 main : Program Never Model Msg
@@ -25,19 +27,25 @@ main =
 
 
 type alias Model =
-    { quote : String 
+    { username : String
+    , password : String
+    , token : String
+    , quote : String
+    , errorMsg : String
     }
     
 init : (Model, Cmd Msg)
 init =
-    ( Model "", fetchRandomQuoteCmd )
+    ( Model "" "" "" "" "", fetchRandomQuoteCmd )
 
 
 
 {-
    UPDATE
    * API routes
-   * GET
+   * GET and POST
+   * Encode request body
+   * Decode responses
    * Messages
    * Update case
 -}
@@ -52,6 +60,11 @@ api =
 randomQuoteUrl : String
 randomQuoteUrl =
     api ++ "api/random-quote"
+
+
+registerUrl : String
+registerUrl =
+    api ++ "users"
 
 
 
@@ -79,12 +92,68 @@ fetchRandomQuoteCompleted model result =
 
 
 
+
+-- Encode user to construct POST request body (for Register and Log In)
+
+
+userEncoder : Model -> Encode.Value
+userEncoder model =
+    Encode.object
+        [ ( "username", Encode.string model.username )
+        , ( "password", Encode.string model.password )
+        ]
+
+
+
+-- POST register / login request
+
+
+authUser : Model -> String -> Http.Request String
+authUser model apiUrl =
+    let
+        body =
+            model
+                |> userEncoder
+                |> Http.jsonBody
+    in
+        Http.post apiUrl body tokenDecoder
+
+
+authUserCmd : Model -> String -> Cmd Msg
+authUserCmd model apiUrl =
+    Http.send GetTokenCompleted (authUser model apiUrl)
+
+
+
+-- Decode POST response to get token
+
+
+tokenDecoder : Decoder String
+tokenDecoder =
+    Decode.field "id_token" Decode.string
+
+
+getTokenCompleted : Model -> Result Http.Error String -> ( Model, Cmd Msg )
+getTokenCompleted model result =
+    case result of
+        Ok newToken ->
+            ( { model | token = newToken, password = "", errorMsg = "" } |> Debug.log "got new token", Cmd.none )
+
+        Err error ->
+            ( { model | errorMsg = (toString error) }, Cmd.none )
+
+
+
 -- Messages	
 
 
 type Msg
     = GetQuote
     | FetchRandomQuoteCompleted (Result Http.Error String)
+    | SetUsername String
+    | SetPassword String
+    | ClickRegisterUser
+    | GetTokenCompleted (Result Http.Error String)
 
 
 -- Update
@@ -99,23 +168,93 @@ update msg model =
         FetchRandomQuoteCompleted result ->
             fetchRandomQuoteCompleted model result
 
+        SetUsername username ->
+            ( { model | username = username }, Cmd.none )
+
+        SetPassword password ->
+            ( { model | password = password }, Cmd.none )
+
+        ClickRegisterUser ->
+            ( model, authUserCmd model registerUrl )
+
+        GetTokenCompleted result ->
+            getTokenCompleted model result
 
 
 {-
    VIEW
    * Get a quote
+   * Register
 -}
 
 
 view : Model -> Html Msg
 view model =
-    div [ class "container" ] [
-        h2 [ class "text-center" ] [ text "Chuck Norris Quotes" ]
-        , p [ class "text-center" ] [
-            button [ class "btn btn-success", onClick GetQuote ] [ text "Grab a quote!" ]
-        ]
-        -- Blockquote with quote
-        , blockquote [] [ 
-            p [] [text model.quote] 
-        ]
-    ]
+    let
+        -- Is the user logged in?
+        loggedIn : Bool
+        loggedIn =
+            if String.length model.token > 0 then
+                True
+            else
+                False
+
+        -- If the user is logged in, show a greeting; if logged out, show the login/register form
+        authBoxView =
+            let
+                -- If there is an error on authentication, show the error alert
+                showError : String
+                showError =
+                    if String.isEmpty model.errorMsg then
+                        "hidden"
+                    else
+                        ""
+
+                -- Greet a logged in user by username
+                greeting : String
+                greeting =
+                    "Hello, " ++ model.username ++ "!"
+            in
+                if loggedIn then
+                    div [ id "greeting" ]
+                        [ h3 [ class "text-center" ] [ text greeting ]
+                        , p [ class "text-center" ] [ text "You have super-secret access to protected quotes." ]
+                        ]
+                else
+                    div [ id "form" ]
+                        [ h2 [ class "text-center" ] [ text "Log In or Register" ]
+                        , p [ class "help-block" ] [ text "If you already have an account, please Log In. Otherwise, enter your desired username and password and Register." ]
+                        , div [ class showError ]
+                            [ div [ class "alert alert-danger" ] [ text model.errorMsg ]
+                            ]
+                        , div [ class "form-group row" ]
+                            [ div [ class "col-md-offset-2 col-md-8" ]
+                                [ label [ for "username" ] [ text "Username:" ]
+                                , input [ id "username", type_ "text", class "form-control", Html.Attributes.value model.username, onInput SetUsername ] []
+                                ]
+                            ]
+                        , div [ class "form-group row" ]
+                            [ div [ class "col-md-offset-2 col-md-8" ]
+                                [ label [ for "password" ] [ text "Password:" ]
+                                , input [ id "password", type_ "password", class "form-control", Html.Attributes.value model.password, onInput SetPassword ] []
+                                ]
+                            ]
+                        , div [ class "text-center" ]
+                            [ button [ class "btn btn-link", onClick ClickRegisterUser ] [ text "Register" ]
+                            ]
+                        ]
+    in
+        div [ class "container" ]
+            [ h2 [ class "text-center" ] [ text "Chuck Norris Quotes" ]
+            , p [ class "text-center" ]
+                [ button [ class "btn btn-success", onClick GetQuote ] [ text "Grab a quote!" ]
+                ]
+              -- Blockquote with quote
+            , blockquote []
+                [ p [] [ text model.quote ]
+                ]
+            , div [ class "jumbotron text-left" ]
+                [ -- Login/Register form or user greeting
+                  authBoxView
+                ]
+            ]
